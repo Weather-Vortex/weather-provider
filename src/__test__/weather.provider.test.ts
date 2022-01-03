b7440e58967f1435d6f7141d5f53ddac8ea3d252
Daniele Tentoni <d.tentoni@wedoit.io>
2022-01-03 15:46:55 +0100

    test(outputs): fix expected output for different node versions
    In Node 14 TypeError expected message of URL construction include wrong URL, but in Node 16 it is missing.

--- src/__test__/weather.provider.test.ts
+++ src/__test__/weather.provider.test.ts
@@ -2,9 +2,22 @@ import { validProtocols, WeatherProvider } from '../index';
 
 const rightCreatedProviderName = 'Weather Provider';
 
+const runOnSpecificNodeVersion: (desiredVersion: string) => boolean = (
+  desiredVersion,
+) => {
+  const nodeVersion = process.versions.node.match(/^(\d+)\.\d+\.\d+$/);
+  return nodeVersion?.length == 2 && nodeVersion[1] == desiredVersion;
+};
+
 const construction = () => new WeatherProvider('aaa', 'tok', 'val');
 test('Fail creating a provider without a well formed URL', () => {
-  expect(construction).toThrow(TypeError('Invalid URL'));
+  const nodeVersion = process.versions.node.match(/^(\d+)\.\d+\.\d+$/);
+  console.info(process.versions.node, nodeVersion);
+  if (runOnSpecificNodeVersion('14')) {
+    expect(construction).toThrow(TypeError('Invalid URL: aaa'));
+  } else {
+    expect(construction).toThrow(TypeError('Invalid URL'));
+  }
 });
 
 test('Create a provider with a well formed URL', () => {
